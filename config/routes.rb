84bd8ad84117a2367691198ec05030ce1f2f65e7
kouichi shinkokura <stndard809@gmail.com>
2022-12-23 23:56:24 +0900

    変更3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :pictures do 
-    collection do
-      post :confirm
-    end
-  end
+  resources :pictures 
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
