ef32c0af89e2c3bf8073b228ebb2c4172480ac2d
kouichi shinkokura <stndard809@gmail.com>
2022-12-24 13:44:30 +0900

    8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to 'users#new'
+  root 'users#new'
   resources :pictures 
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
