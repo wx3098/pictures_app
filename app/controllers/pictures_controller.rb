202e8b9cf44ab16747b61961fc8ef583ffe26931
kouichi shinkokura <stndard809@gmail.com>
2022-12-25 00:39:13 +0900

    修正2

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -11,7 +11,7 @@ class PicturesController < ApplicationController
     def create
         @picture = Picture.new(picture_params)
       if @picture.save
-      PictureMailer.picture_mail(@picture).deliver
+    #   PictureMailer.picture_mail(@picture).deliver
       redirect_to pictures_path, notice: '投稿しました！'
       else
         render :new
